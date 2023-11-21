35555a03e5362962b8667efdd53daa1e465d07c4
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-11-21 17:56:46 +0100

    A new `PUT` method `PutBeaver` has been added to update a beaver's details.

--- API/Controllers/BeaversController.cs
+++ API/Controllers/BeaversController.cs
@@ -1,7 +1,9 @@
 ﻿using API.Converters;
 using API.Models;
 using BeaversDB;
+using BeaversDB.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -14,5 +16,44 @@ public class BeaversController(AnimalContext context) : ControllerBase
 	public ActionResult<IEnumerable<BeaverModel>> GetBeavers()
 	{
 		return context.Beavers.Select(b => b.ToModel()).ToList();
+	}
+	// PUT: api/Beavers/5
+	[HttpPut("{id}")]
+	public async Task<IActionResult> PutBeaver(int id, BeaverModel beaverModel)
+	{
+		if (id != beaverModel.Id)
+		{
+			return BadRequest();
+		}
+		var beaver = await context.Beavers.FindAsync(id);
+		if (beaver == null)
+		{
+			return NotFound();
+		}
+		beaver.Name = beaverModel.Name;
+		beaver.Fluffiness = Enum.Parse<FluffinessEnum>(beaverModel.Fluffiness);
+		beaver.Size = beaverModel.Size;
+		// Update other properties
+		try
+		{
+			await context.SaveChangesAsync();
+		}
+		catch (DbUpdateConcurrencyException)
+		{
+			if (!BeaverExists(id))
+			{
+				return NotFound();
+			}
+			else
+			{
+				throw;
+			}
+		}
+		return NoContent();
+	}
+
+	private bool BeaverExists(int id)
+	{
+		return context.Beavers.Any(e => e.Id == id);
 	}
 }
