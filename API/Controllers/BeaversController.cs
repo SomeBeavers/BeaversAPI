4fecc04b949c67c1d4b729243317d5644f017c5e
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-11-22 11:44:18 +0100

    The most significant change is the addition of a new `PostBeaver` method. This method allows the creation of a new beaver entity and its addition to the `Beavers` context.

--- API/Controllers/BeaversController.cs
+++ API/Controllers/BeaversController.cs
@@ -11,49 +11,50 @@ namespace API.Controllers;
 [Route("api/[controller]")]
 public class BeaversController(AnimalContext context) : ControllerBase
 {
-	// GET: api/Beavers
-	[HttpGet]
-	public ActionResult<IEnumerable<BeaverModel>> GetBeavers()
-	{
-		return context.Beavers.Select(b => b.ToModel()).ToList();
-	}
-	// PUT: api/Beavers/5
-	[HttpPut("{id}")]
-	public async Task<IActionResult> PutBeaver(int id, BeaverModel beaverModel)
-	{
-		if (id != beaverModel.Id)
-		{
-			return BadRequest();
-		}
-		var beaver = await context.Beavers.FindAsync(id);
-		if (beaver == null)
-		{
-			return NotFound();
-		}
-		beaver.Name = beaverModel.Name;
-		beaver.Fluffiness = Enum.Parse<FluffinessEnum>(beaverModel.Fluffiness);
-		beaver.Size = beaverModel.Size;
-		// Update other properties
-		try
-		{
-			await context.SaveChangesAsync();
-		}
-		catch (DbUpdateConcurrencyException)
-		{
-			if (!BeaverExists(id))
-			{
-				return NotFound();
-			}
-			else
-			{
-				throw;
-			}
-		}
-		return NoContent();
-	}
+    // GET: api/Beavers
+    [HttpGet]
+    public ActionResult<IEnumerable<BeaverModel>> GetBeavers()
+    {
+        return context.Beavers.Select(b => b.ToModel()).ToList();
+    }
 
-	private bool BeaverExists(int id)
-	{
-		return context.Beavers.Any(e => e.Id == id);
-	}
+    // PUT: api/Beavers/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutBeaver(int id, BeaverModel beaverModel)
+    {
+        if (id != beaverModel.Id) return BadRequest();
+        var beaver = await context.Beavers.FindAsync(id);
+        if (beaver == null) return NotFound();
+        beaver.Name = beaverModel.Name;
+        beaver.Fluffiness = Enum.Parse<FluffinessEnum>(beaverModel.Fluffiness);
+        beaver.Size = beaverModel.Size;
+        // Update other properties
+        try
+        {
+            await context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!BeaverExists(id))
+                return NotFound();
+            throw;
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/Beavers
+    [HttpPost]
+    public async Task<ActionResult<BeaverModel>> PostBeaver(BeaverModel beaverModel)
+    {
+        var beaver = beaverModel.ToEntity();
+        context.Beavers.Add(beaver);
+        await context.SaveChangesAsync();
+        return CreatedAtAction("GetBeaver2", new {id = beaver.Id}, beaver.ToModel());
+    }
+
+    private bool BeaverExists(int id)
+    {
+        return context.Beavers.Any(e => e.Id == id);
+    }
 }
