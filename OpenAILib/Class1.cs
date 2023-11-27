1244f7cf5ee465df1416ea030ac18cc9a82121ab
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-11-27 12:01:32 +0100

    upd

--- OpenAILib/Class1.cs
+++ OpenAILib/Class1.cs
@@ -4,7 +4,7 @@ public class Class1
 {
     public async Task ConnectToOpenAI()
     {
-        var api = new OpenAIAPI(new APIAuthentication("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"));
+        var api = new OpenAIAPI(new APIAuthentication(""));
         var result = await api.Completions.CreateCompletionAsync("I would like to ", Model.DefaultModel);
         Console.WriteLine(result.Completions[0].Text.Trim());
     }
