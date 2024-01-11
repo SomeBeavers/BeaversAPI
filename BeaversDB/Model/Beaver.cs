bd34ea012194b791530356ad957d48a1d69b94a9
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-01-11 11:23:54 +0100

    Update .editorconfig

--- BeaversDB/Model/Beaver.cs
+++ BeaversDB/Model/Beaver.cs
@@ -2,22 +2,22 @@
 
 //[Table("Beaver")]
 public class Beaver : Animal
-{
-    public FluffinessEnum   Fluffiness  { get; set; }
-    public int              Size        { get; set; }
-    public List<AnimalClub> AnimalClubs { get; set; }
-    
+    {
+    public FluffinessEnum   Fluffiness    { get; set; }
+    public int              Size          { get; set; }
+    public List<AnimalClub> AnimalClubs   { get; set; }
+    public List<Food>       FavoriteFoods { get; set; }
 
 
     public override string ToString()
-    {
+        {
         return @$"{base.ToString()} Beaver: Fluffiness = {this.Fluffiness} Size = {this.Size}";
+        }
     }
-}
 
 public enum FluffinessEnum
-{
+    {
     NotFluffy,
     Fluffy,
     VeryFluffy
-}
+    }
