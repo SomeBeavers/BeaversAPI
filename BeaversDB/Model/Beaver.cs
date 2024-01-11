1e4e55a6e0ca3f9f51915c23c4d08dea4e1a43d2
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-01-11 12:07:52 +0100

    Upd

--- BeaversDB/Model/Beaver.cs
+++ BeaversDB/Model/Beaver.cs
@@ -2,22 +2,31 @@
 
 //[Table("Beaver")]
 public class Beaver : Animal
-    {
-    public FluffinessEnum   Fluffiness    { get; set; }
-    public int              Size          { get; set; }
-    public List<AnimalClub> AnimalClubs   { get; set; }
-    public List<Food>       FavoriteFoods { get; set; }
+{
+	public FluffinessEnum Fluffiness { get; set; }
+	public int Size { get; set; }
+	public List<AnimalClub> AnimalClubs { get; set; }
 
 
-    public override string ToString()
-        {
-        return @$"{base.ToString()} Beaver: Fluffiness = {this.Fluffiness} Size = {this.Size}";
-        }
-    }
+	public override string ToString()
+	{
+	
+		return @$"{base.ToString()} Beaver: Fluffiness = {this.Fluffiness} Size = {this.Size}";
+	}
+
+
+	public void DoSomething()
+	{
+		AnimalClub animalClub = new AnimalClub();
+		animalClub.AnimalId = this.Id;
+		animalClub.ClubId = 1;
+		animalClub.PublicationDate = DateTime.Now;
+	}
+}
 
 public enum FluffinessEnum
-    {
-    NotFluffy,
-    Fluffy,
-    VeryFluffy
-    }
+{
+	NotFluffy,
+	Fluffy,
+	VeryFluffy
+}
