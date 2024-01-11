32ed0315d3cdad09fa6c87d7dc0e98980a0e8dc9
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-01-11 12:51:40 +0100

    Update .editorconfig

--- API/Converters/BeaverExtensions.cs
+++ API/Converters/BeaverExtensions.cs
@@ -5,36 +5,30 @@ using System.Net;
 namespace API.Converters;
 
 public static class BeaverExtensions
-{
-    public static BeaverModel ToModel(this Beaver beaver)
     {
-        return new BeaverModel
+    public static BeaverModel ToModel(this Beaver beaver)
         {
-            Id = beaver.Id,
-            Name = beaver.Name,
-            Age = beaver.Age,
-            Fluffiness = beaver.Fluffiness.ToString(),
-            Size = beaver.Size
-        };
-    }
+        return new BeaverModel
+            {
+            };
+        }
 
     public static Beaver ToEntity(this BeaverModel beaver)
-    {
-        return new Beaver
         {
+        return new Beaver
+            {
             Name = beaver.Name,
             Fluffiness = Enum.Parse<FluffinessEnum>(beaver.Fluffiness),
             Size = beaver.Size,
             Age = beaver.Age,
             IpAddress = IPAddress.Parse("127.0.0.1")
-        };
-    }
+            };
+        }
 
     public static string GetFluffiness(this BeaverModel beaver)
-    {
-        return beaver.Fluffiness switch
         {
-            
+        return beaver.Fluffiness switch
+            {
+            };
         }
     }
-}
